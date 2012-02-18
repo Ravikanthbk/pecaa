796c78382e275720c8142c57cc25fb452e706b25
Manikandan K <manikandan.nitt@gmail.com>
2012-02-18 09:55:12 +0530

    Forgot password changes

--- config/environment.rb
+++ config/environment.rb
@@ -6,13 +6,12 @@ Mystore3::Application.initialize!
 
 ActiveRecord::Base.include_root_in_json = true
 
-require 'smtp_tls'
-
-ActionMailer::Base.server_settings = {
-  :address => "smtp.gmail.com",
-  :port => "587",
-  :domain => "localhost.localdomain",
-  :authentication => :plain,
-  :user_name => "pecaa.spree",
-  :password => "pecaa123"
-}
+ActionMailer::Base.smtp_settings = {
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "GMAIL",
+    :authentication => :plain,
+    :user_name => "pecaa.spree",
+    :password => "pecaa123"
+  }
