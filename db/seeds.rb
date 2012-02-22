90f05f0e2353b7c95bb7c3dc365ae1b831bf3e32
RaguR <rajesh@ras.beamtele.net>
2012-02-23 02:23:33 +0530

    [MODIFY] Seed data script modified for User base/Listing screen

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
+Role.delete_all
+User.delete_all
 
 role = Role.create(:name=>"admin")
-User.create(:email=>"admin-pecaa@gmail.com",:username=>"admin-pecaa",:password=>"pecaa123",:role=>role )
+user_role = Role.create(:name=>"user")
+User.create(:email=>"admin-pecaa@gmail.com",:username=>"admin-pecaa",:password=>"pecaa123",:role=>role).roles = [role]
+20.times do |i|
+  u = User.create(:email=>"test#{i}@gmail.com",:username=>"test#{i}", :password=>"test#{i}",:role=>user_role)
+  u.roles = [user_role]
+end
