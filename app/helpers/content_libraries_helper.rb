93508c51777ec0ea2fed30a7743e71c2f7629c7e
ravikanth.bk <ravikanth.bk@gmail.com>
2012-03-02 23:20:56 +0530

    Added View link to content library

--- app/helpers/content_libraries_helper.rb
+++ app/helpers/content_libraries_helper.rb
@@ -3,7 +3,7 @@ module ContentLibrariesHelper
   def display_content(content)
     if content.source_type == 'Image'
       image = content.source.upload.url(:medium).split('?')[0]
-      "<a href=#{image} rel='facebox'><img src='images/icon-eye.png' /></a>".html_safe
+      "<a href=#{image} rel='facebox'><img src='images/icon-eye.png' title='View' /></a>".html_safe
     end
   end
 
