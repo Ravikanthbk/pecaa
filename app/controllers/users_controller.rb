59bea23c4c69dc1aa4ecb4528804cf764f344c10
RajeshV <veedhirajesh@gmail.com>
2012-03-04 15:27:16 +0530

    [FIX] USER listing page links and Site CSS fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.json { render :json => @users }
       format.xml  { render :xml => @users }
-      format.html { render :layout => 'listing'}
+      format.html
     end
   end
  
